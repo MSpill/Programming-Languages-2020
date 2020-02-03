737ed245a812d8335be68782c406bf756d18d8b7
Matthew Spillman <matthewspillman@usmb-2017l1460.westminster.net>
2020-02-03 11:17:26 -0500

    Finished myFirstPractice.scm

--- SICP/Practice/Section1/myFirstPractice.scm
+++ SICP/Practice/Section1/myFirstPractice.scm
@@ -2,4 +2,14 @@
     (println "AUTHOR: Matthew Spillman matthewspillman@westminster.net")
 )
 
-(println "Hello world!")
+; Phone number is 1 (404) 952 3100
+(inspect (- 2 1))
+(inspect (/ 808 2))
+(inspect (+ 950 2))
+(inspect (* 50 62))
+
+; Compound statements should give 11, 15, -22, and 2
+(inspect (- (+ (* 3 4) (/ 4 2)) 3))
+(inspect (* (+ 1 2) 5))
+(inspect (- 3 (* (+ 2 3) 5)))
+(inspect (/ 4 (+ 1 (- 2 1))))
