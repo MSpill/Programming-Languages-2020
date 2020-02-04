1afda8a498b57c0274088e9c24a76d6eda1a1c0a
Matthew Spillman <matthewspillman@usmb-2017l1460.westminster.net>
2020-02-04 08:32:05 -0500

    Improved myFirstPractice and reading2Notes

--- SICP/Practice/Section1/myFirstPractice.scm
+++ SICP/Practice/Section1/myFirstPractice.scm
@@ -2,14 +2,22 @@
     (println "AUTHOR: Matthew Spillman matthewspillman@westminster.net")
 )
 
-; Phone number is 1 (404) 952 3100
+; Phone number is 1 (404)-952-3100
 (inspect (- 2 1))
 (inspect (/ 808 2))
 (inspect (+ 950 2))
 (inspect (* 50 62))
 
-; Compound statements should give 11, 15, -22, and 2
+; Compound expressions
+
+; ((3 * 4) + (4 / 2)) - 3 = 11
 (inspect (- (+ (* 3 4) (/ 4 2)) 3))
-(inspect (* (+ 1 2) 5))
+
+; (1 + (1 - 2)) * 5 = 0
+(inspect (* (+ 1 (- 1 2)) 5))
+
+; 3 - ((2 + 3) * 5) = -22
 (inspect (- 3 (* (+ 2 3) 5)))
+
+; 4 / (1 + (2 - 1)) = 2
 (inspect (/ 4 (+ 1 (- 2 1))))
--- SICP/Practice/Section1/reading2Notes.scm
+++ SICP/Practice/Section1/reading2Notes.scm
@@ -6,7 +6,12 @@
 (define size 2)
 
 ; Reference them later
-(inspect (+ size size))
+(inspect (* 5 size))
+(define pi 3.14159)
+(define radius 10)
+(inspect (* pi (* radius radius)))
+(define circumference (* 2 pi radius))
+(inspect circumference)
 
 ; Evaluating combinations is recursive
 ; 1. Evaluate all the subexpressions
@@ -16,9 +21,15 @@
 ; You can define procedures with the define keyword
 
 (define (square x) (* x x))
-(inspect (square 5))
+(inspect (square 21))
+(inspect (square (+ 2 5)))
+(inspect (square (square 3)))
 
 ; You can make compound procedures
 
 (define (sum-of-squares x y) (+ (square x) (square y)))
 (inspect (sum-of-squares 3 4))
+
+(define (f a)
+    (sum-of-squares (+ a 1) (* a 2)))
+(inspect (f 5))
