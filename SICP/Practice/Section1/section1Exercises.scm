76369135382adcfa4c7448a617432e5276a4ef39
Matthew Spillman <matthewspillman@usmb-2017l1460.westminster.net>
2020-02-12 13:10:21 -0500

    Finished 1.6

--- SICP/Practice/Section1/section1Exercises.scm
+++ SICP/Practice/Section1/section1Exercises.scm
@@ -22,8 +22,10 @@
     )
 )
 
-; Auxiliary sum of squares function
-(define (sum_of_squares a b) (+ (* a a) (* b b)))
+; Auxiliary functions
+(define (sum_of_squares a b) (+ (square a) (square b)))
+
+(define (square x) (* x x))
 
 ; Should be 13
 (inspect (sum_larger_squares 2 1 3))
@@ -38,3 +40,21 @@
 ; which evaluates to + if b is positive and - if b is negative. It then
 ; applies that operator to operands a and b.
 
+; Exercise 1.6
+
+; Using the new if statement would cause the sqrt-iter procedure to be carried out
+; in an infinite loop until the program ran out of memory. This is because a normal
+; if statement first evaluates the predicate, then if the predicate is true it evaluates
+; the consequent and returns it. Otherwise it evaluates the alternative and returns it.
+; Since new-if is a normal procedure the order is different. First the predicate, consequent
+; and alternative are all evaluated, then either the consequent or the alternative is returned.
+; In the new sqrt-iter function using new-if, the alternative is another call to sqrt-iter.
+; Using a normal if statement, this would only be evaluated if necessary. Using new-if,
+; it is automatically evaluated, so each call to sqrt-iter soon calls sqrt-iter again
+; no matter what the parameters are. This results in endless recursion which eventually
+; causes the program to run out of memory.
+
+(define (sqrt-iter guess x)
+    (new-if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x))) ; This would get evaluated regardless of guess or x
