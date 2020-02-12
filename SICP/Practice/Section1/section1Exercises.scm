f87197d24dca61bcc29e8c1ac143e1f9c78d9a44
Matthew Spillman <matthewspillman@usmb-2017l1460.westminster.net>
2020-02-12 12:40:15 -0500

    Finished 1.4

--- SICP/Practice/Section1/section1Exercises.scm
+++ SICP/Practice/Section1/section1Exercises.scm
@@ -13,6 +13,7 @@
 
 ; Exercise 1.3
 
+; Define larger squares function by figuring out smallest value and squaring the other two
 (define (sum_larger_squares a b c)
     (cond 
         ((and (< c a) (< c b)) (sum_of_squares a b))
@@ -21,9 +22,19 @@
     )
 )
 
+; Auxiliary sum of squares function
 (define (sum_of_squares a b) (+ (* a a) (* b b)))
 
 ; Should be 13
 (inspect (sum_larger_squares 2 1 3))
 
 ; Exercise 1.4
+
+(define (a_plus_abs_b a b)
+    ((if (> b 0) + -) a b))
+
+; This procedure returns a+b if b is positive and a-b otherwise.
+; It does this by turning the operator on line 2 (34) into an if statement
+; which evaluates to + if b is positive and - if b is negative. It then
+; applies that operator to operands a and b.
+
