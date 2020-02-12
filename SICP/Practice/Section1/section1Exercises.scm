bb32278ac22a5b541c4be665a6a3cc9f2cd6a74d
Matthew Spillman <matthewspillman@usmb-2017l1460.westminster.net>
2020-02-12 12:32:47 -0500

    Finished 1.3

--- SICP/Practice/Section1/section1Exercises.scm
+++ SICP/Practice/Section1/section1Exercises.scm
@@ -13,3 +13,17 @@
 
 ; Exercise 1.3
 
+(define (sum_larger_squares a b c)
+    (cond 
+        ((and (< c a) (< c b)) (sum_of_squares a b))
+        ((and (< b a) (< b c)) (sum_of_squares a c))
+        (else (sum_of_squares b c))
+    )
+)
+
+(define (sum_of_squares a b) (+ (* a a) (* b b)))
+
+; Should be 13
+(inspect (sum_larger_squares 2 1 3))
+
+; Exercise 1.4
