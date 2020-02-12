d2eb6e397ff162a3bcb7cd55ddca763a3c4171ed
Matthew Spillman <matthewspillman@usmb-2017l1460.westminster.net>
2020-02-12 12:20:44 -0500

    Finished 1.2

---
+++ SICP/Practice/Section1/section1Exercises.scm
@@ -0,0 +1,15 @@
+(define (main)
+    (println "AUTHOR: Matthew Spillman matthewspillman@westminster.net")
+)
+
+; Exercise 1.2
+; Expected answer: -0.246666667
+
+(inspect
+(/
+    (+ 5 4 (- 2 (- 3 (+ 6 (/ 4.0 5.0)))))
+    (* 3 (- 6 2) (- 2 7))
+))
+
+; Exercise 1.3
+
