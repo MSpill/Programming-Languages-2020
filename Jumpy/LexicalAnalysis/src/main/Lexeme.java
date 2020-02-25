33056ddebf176169a99de2197bf4b27bdf5b9f7a
Matthew Spillman <matthewspillman@westminster.net>
2020-02-25 09:38:34 -0500

    Formatted grammar and started lexer

--- Jumpy/LexicalAnalysis/src/main/Lexeme.java
+++ Jumpy/LexicalAnalysis/src/main/Lexeme.java
@@ -1,1 +1,6 @@
-
+class Lexeme {
+    String type;
+    String string;
+    int integer;
+    double real;
+}
--- Jumpy/LexicalAnalysis/src/main/Lexer.java
+++ Jumpy/LexicalAnalysis/src/main/Lexer.java
@@ -1,1 +1,17 @@
+import java.io.*;
 
+class Lexer {
+    private PushbackReader input;
+
+    Lexer(String inputFilePath) {
+        try {
+            System.out.println("Lexing " + inputFilePath);
+
+            File inputFile = new File(inputFilePath);
+            input = new PushbackReader(new FileReader(inputFile));
+        } catch (FileNotFoundException error) {
+            System.out.println("ERROR: Could not find file " + inputFilePath + " for lexing. " + error);
+        }
+    }
+
+}
