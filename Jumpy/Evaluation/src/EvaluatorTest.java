1b2a25969902886b0860ca71d03a75d791f1b9b1
Matthew Spillman <matthewspillman@westminster.net>
2020-05-20 12:10:23 -0400

    More complex test file works

--- Jumpy/Evaluation/src/EvaluatorTest.java
+++ Jumpy/Evaluation/src/EvaluatorTest.java
@@ -6,7 +6,6 @@ public class EvaluatorTest {
             Recognizer recognizer = new Recognizer(testFile);
             Lexeme parseTree = recognizer.run();
             Evaluator evaluator = new Evaluator();
-            parseTree.printTree();
             evaluator.run(parseTree);
         } catch (Exception e) {
             System.out.println(e);
